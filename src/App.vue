ac65df3de190d9643344e710f94e4c5cdd26321a
tech7ie <maksim162005@mail.ru>
2025-03-22 02:38:12 +0500

    init

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+
+</script>
+
+<template>
+  <main>
+    <div class="malevich-container">
+      <div class="malevich-area"></div>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+.malevich-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+}
+
+.malevich-area {
+  background-color: black;
+  height: 50%;
+  aspect-ratio: 1/1;
+}
+</style>
